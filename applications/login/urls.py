4e6f4cc3ca3073ddf5a1af0dc67c11fd507a86e7
Pablo Acosta <acosta13233@hotmail.com>
2023-07-20 17:35:39 -0600

    initial commit
    commit inicial se agrego la base del proyecto

---
+++ applications/login/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from applications.login.views import *
+
+app_name = 'login_app'
+
+urlpatterns = [
+    
+]
---
+++ applications/ticket/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from applications.ticket.views import *
+
+app_name = 'ticket_app'
+
+urlpatterns = [
+    
+]
